1c7cc1cb9347cc8fc075da571946d2b3b30e825e
Shalini648404 <shalinibaswaraju@gmail.com>
2025-05-02 10:22:32 +0530

    Added custom arrow to gender

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
 
 export default App;
 */
+/* 
 import React, { useState } from 'react';
 import PatientForm from './components/PatientForm';
 import SqlQuery from './components/SqlQuery';
@@ -32,7 +33,35 @@ function App() {
           {darkMode ? <FaSun /> : <FaMoon />}
         </button>
       </header>
+      <main>
+        <PatientForm />
+        <hr />
+        <SqlQuery />
+      </main>
+    </div>
+  );
+}
 
+export default App;*/
+
+
+import React, { useState } from 'react';
+import PatientForm from './components/PatientForm';
+import SqlQuery from './components/SqlQuery';
+import './App.css';
+import { FaMoon, FaSun } from 'react-icons/fa';
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+    <div className={`app-container ${darkMode ? 'dark' : 'light'}`}>
+      <header className="app-header">
+        <h1 className="main-heading">Patient Registration App</h1>
+        <button className="toggle-btn" onClick={() => setDarkMode(prev => !prev)}>
+          {darkMode ? <FaSun /> : <FaMoon />}
+        </button>
+      </header>
       <main>
         <PatientForm />
         <hr />
